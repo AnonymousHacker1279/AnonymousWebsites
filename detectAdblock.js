1370d36a8b9d79bdee8682c391ddd0519b33b962
Anonymous Hacker <48810167+AnonymousHacker1279@users.noreply.github.com>
2019-09-06 20:34:05 -0400

    Restored Anonymous Websites Backup

---
+++ detectAdblock.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    if($("#wrapfabtest").height() < 0) {
+        alert('We detect that you are using an adblock. Please consider unblocking us as small ads help us keep the website running. ');
+    }
+});
+
